9fbd313bf3b89d99f3b281d67e557b0b37143ce2
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-08-07 10:49:32 +0000

    complete task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,14 @@ from .auth import Auth
 
 class BasicAuth(Auth):
     """Basic Authentication class"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Extracts the Base64 part of the Authorization
+        for Basic Authentication"""
+        if authorization_header is None:
+            return None
+        if type(authorization_header) not in [str]:
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header.split(' ')[1]
