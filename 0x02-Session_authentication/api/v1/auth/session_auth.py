895c4cc40963271417bb2415f9655ce86ec48810
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-08-10 09:58:33 +0000

    complete task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,19 @@
 """Module contains a SessionAuth class
 That Inherits from Auth Class"""
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Session Authentication class"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session ID for a user_id"""
+        if user_id is None:
+            return None
+        if type(user_id) not in [str]:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
