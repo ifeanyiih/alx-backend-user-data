fbfbe106d9b14827b88f73428ee2700d0b9532cf
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-08-10 09:31:05 +0000

    complete task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module contains a SessionAuth class
+That Inherits from Auth Class"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """Session Authentication class"""
+    pass
