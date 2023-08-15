719325be59c10ff910763510557b6356fc7b50f1
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-08-15 12:37:15 +0000

    complete task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
 """Module contains a _hash_password function"""
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """Initializes the instance"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers a User in the database"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound as e:
+            hashed_pass = _hash_password(password)
+            user = self._db.add_user(email, hashed_pass)
+            return user
+        else:
+            raise ValueError(f"User {email} already exists")
 
 
 def _hash_password(password: str) -> bytes:
