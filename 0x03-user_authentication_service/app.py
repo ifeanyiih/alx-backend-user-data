e014a80114bf9c80f1ce010488b42a5f91db8435
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-08-16 21:15:25 +0000

    complete task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """A basic Flask app"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -11,6 +11,23 @@ AUTH = Auth()
 def rootRoute():
     """The root Route of the app"""
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """implements the POST /sessions route
+    Create session cookies for users"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email or not password:
+        abort(401)
+    valid_login = AUTH.valid_login(email, password)
+    if not valid_login:
+        abort(401)
+    session_id = AUTH.create_session(email)
+    res = jsonify({"email": email, "message": "logged in"})
+    res.set_cookie('session_id', session_id)
+    return res, 200
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
