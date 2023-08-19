540bd108d4fa42dcde939099b1036cf450861c81
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-08-19 13:48:09 +0000

    remove exception

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -60,7 +60,7 @@ class Auth:
             session_id = _generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
             return session_id
-        except (NoResultFound, Exception):
+        except NoResultFound:
             return None
 
     def get_user_from_session_id(self, session_id: str) -> User:
